48bd7ad523ce990931bfeab31618877dc710e4b9
钟俊杰 <123217851+ZephyrSober@users.noreply.github.com>
2026-04-02 19:26:05 +0800

    Revert "Add manifest-based training data pipeline"

--- code/train/src/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .config import infer_project_root, load_experiment_config, load_yaml_config, resolve_config_path
-
-__all__ = ["infer_project_root", "load_experiment_config", "load_yaml_config", "resolve_config_path"]
